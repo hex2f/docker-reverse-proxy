71d94db79795d18769d7739db93bac9130ba9395
Leah Lundqvist <leah@pigeon.sh>
2021-08-25 16:32:58 +0200

    add error catcher

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,13 @@ import ContainerManager from '~/container-manager'
 import HTTPServer from './http-server'
 import SSHServer from './ssh-server'
 import HTTPSServer from './https-server'
+import log from './log'
 
 const manager = new ContainerManager('./containers.yml')
 const https = new HTTPSServer('./ssl.yml', manager, 443)
 const http = new HTTPServer(manager, 80)
 const ssh = new SSHServer('./id_rsa', 22, manager)
+
+process.on('uncaughtException', function(err) {
+  log.error(err)
+})
