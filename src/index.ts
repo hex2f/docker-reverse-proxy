5eabf81a99a2f486146eeb916160811762cec852
Leah Lundqvist <leah@pigeon.sh>
2021-11-23 01:02:25 +0100

    remove error log

--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,4 @@ const https = new HTTPSServer('./ssl.yml', manager, 443)
 const http = new HTTPServer(manager, 80)
 const ssh = new SSHServer('./id_rsa', 22, manager)
 
-process.on('uncaughtException', function (err) {
-  log.error(err)
-})
+process.on('uncaughtException', function (err) { })
